3076a35792b146558781efae1a7f3027f6fb2118
Admin <Admin@DESKTOP-N6RG63K>
2023-04-24 21:10:58 +0530

    commit

---
+++ Dayone/src/org/tnsif/organization/PrimitiveDataType.java
@@ -0,0 +1,14 @@
+package org.tnsif.organization;
+
+public class PrimitiveDataType {
+
+	public static void main(String[] args) {
+		long x = 2356789008L;
+		System.out.println(x);
+		
+		byte y = -125;
+		System.out.println(y);
+
+	}
+
+}
