efedaadd03aadedc142f1944a0b52b2a590b3c34
Admin <Admin@DESKTOP-N6RG63K>
2023-04-24 21:04:14 +0530

    first commit

---
+++ Dayone/src/org/tnsif/organization/Firstprogeam.java
@@ -0,0 +1,11 @@
+package org.tnsif.organization;
+// program to demonstrate simple java program
+public class Firstprogeam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World !");
+
+	}
+
+}
