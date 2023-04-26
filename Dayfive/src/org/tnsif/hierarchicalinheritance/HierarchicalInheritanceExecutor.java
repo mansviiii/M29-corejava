3b01d23a71c8fc99297559e397b6cf23dee96cbe
Admin <Admin@DESKTOP-N6RG63K>
2023-04-26 14:12:39 +0530

    First Commit

---
+++ Dayfive/src/org/tnsif/hierarchicalinheritance/HierarchicalInheritanceExecutor.java
@@ -0,0 +1,14 @@
+package org.tnsif.hierarchicalinheritance;
+
+public class HierarchicalInheritanceExecutor {
+
+	public static void main(String[] args) {
+		SnowCone s=new SnowCone(12,"SamsungM21");
+		Tiramisu t=new Tiramisu(13, "Realme 10 prp plus");
+		System.out.println(s);
+		System.out.println(t);
+				
+		
+
+	}
+}
