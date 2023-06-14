f36551837a1068d1735340b05a7be2360fea8661
Admin <Admin@DESKTOP-N6RG63K>
2023-06-14 14:38:55 +0530

    commit

---
+++ dayeleven/src/org/tnsif/excentiondemo/ArithamaticException.java
@@ -0,0 +1,29 @@
+package org.tnsif.excentiondemo;
+
+import java.util.Scanner;
+
+public class 45
+{
+
+	public static void main(String[] args) {
+		Scanner s =new Scanner(System.in);
+		System.out.println("Enter a two number");
+		int x =s.nextInt();
+		int y =s.nextInt();
+		//contain an exception code
+		try {
+		System.out.println(x/y);
+		}
+		//catch block handle exception
+		catch(Exception e){
+			System.out.println(e);
+			
+		}
+		
+		finally
+		{
+			System.out.println(" done with");
+		}
+	}
+
+}
