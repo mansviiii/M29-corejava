71dec541f4384e753227554a76bb129c0d1e4380
Admin <Admin@DESKTOP-N6RG63K>
2023-06-17 12:41:50 +0530

    Commit

---
+++ day twelve/src/org/tnsif/collection/ThreadLifeCycle.java
@@ -0,0 +1,9 @@
+package org.tnsif.collection;
+
+public class ThreadLifeCycle {
+
+	public ThreadLifeCycle() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ day twelve/src/org/tnsif/collection/ThreadProgram.java
@@ -0,0 +1,9 @@
+package org.tnsif.collection;
+
+public class ThreadProgram {
+
+	public ThreadProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
